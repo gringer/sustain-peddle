98f9086c5a67da4fa2b6ace56336cb6bc80444c0
David Eccles (gringer) <bioinformatics@gringene.org>
2017-07-29 11:02:17 +1200

    Added second dataset

--- server.R
+++ server.R
@@ -11,12 +11,22 @@ shinyServer(function(input, output) {
     data.file <- list.files("data", full.names = TRUE,
                             pattern = sprintf("^%02d", as.numeric(input$goal)));
     if(length(data.file) > 0){
-      print(data.file);
-      data.df <- read.csv(data.file, row.names=1);
-      data.y <- colSums(data.df, na.rm=TRUE); 
-      data.x <- as.Date(colnames(data.df), format="X%d.%m.%Y"); 
-      plot(data.x, data.y, ylab="Aggregate Value", xlab = "Time",
-           main=data.file, type="b");
+      if(input$goal %in% c(1)){
+        data.df <- read.csv(data.file, row.names=1);
+        data.y <- colSums(data.df, na.rm=TRUE); 
+        data.x <- as.Date(colnames(data.df), format="X%d.%m.%Y"); 
+        plot(data.x, data.y, ylab="Aggregate Value", xlab = "Time",
+             main=data.file, type="b");
+      } else {
+        data.df <- read.csv(data.file, stringsAsFactors = FALSE);
+        data.df$date <- sub("$","/1",data.df$date);
+        data.y <- tapply(data.df$index, data.df$date, mean, na.rm=TRUE);
+        data.x <- as.Date(names(data.y), format="%Y/%m/%d");
+        data.y <- data.y[order(data.x)];
+        data.x <- data.x[order(data.x)];
+        plot(data.x, data.y, ylab="Aggregate Value", xlab="Time",
+             main=data.file, type="b");
+      }
     } else {
       ## dummy plot to demonstrate the user interface
       x <- 1980:2017;
